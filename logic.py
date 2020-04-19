0199ed5e1f5e413033767516b5b3099a7b59c21e
mrityunjai01 <mrityunjaisingh3333@gmail.com>
2020-04-19 11:32:35 +0530

    imported pygame, started sketching the outline

--- logic.py
+++ logic.py
@@ -1,3 +1,6 @@
+import sys
+import os
+import pygame
 xlen = 10
 ylen = 10
 
@@ -20,6 +23,7 @@ class board:
 
         except AttributeError:
             self.init_board()
+
     def restart_game(self):
         try:
             for i in self.arr:
@@ -29,6 +33,7 @@ class board:
         except AttributeError:
             self.init_board()
             self.init_board_cells()
+
     def __str__(self):
         s = ""
         for i in self.arr:
@@ -59,28 +64,33 @@ class cell():
             if self.b.xlen >= i[0] > 0 and self.b.ylen >= i[1] > 0:
                 self.n.append(i)
         self.capacity = len(self.n)-1
+
     def move(self):
         if self.number < self.capacity:
             self.number += 1
         else:
             self.number = 0
+            myside = self.side
+            self.side = None
             for x, y in self.n:
-                a=self.b.arr[x-1][y-1]
-                a.side = self.side
+                a = self.b.arr[x-1][y-1]
+                a.side = myside
                 a.move()
+
+
 def input_move(myside, board1):
     while True:
         b = input('your move: q to quit ')
-        if b=='q':
+        if b == 'q':
             return 1
-        b=b.strip().split(' ')
+        b = b.strip().split(' ')
         if len(b) < 2:
             print('that was one value try again')
             continue
         x = int(b[1])
         y = int(b[0])
-        if x<=board1.xlen and x > 0:
-            if y <= board1.ylen and y >0:
+        if x <= board1.xlen and x > 0:
+            if y <= board1.ylen and y > 0:
                 c = board1.arr[x-1][y-1]
                 if c.side == myside:
                     c.move()
@@ -95,16 +105,69 @@ def input_move(myside, board1):
                 print('your x was not right')
         else:
             print('your x was not right')
+
+
+def count(side, b):
+    cnt = 0
+    for i in b.arr:
+        for j in i:
+            if j.side == side:
+                cnt += 1
+    return cnt
+
+
 def play():
     board1 = board()
     board1.init_board()
     board1.init_board_cells()
     gameExit = False
+    print(board1)
+    if input_move('a', board1):
+        gameExit = True
+    print(board1)
+    if input_move('c', board1):
+        gameExit = True
     while not gameExit:
         print(board1)
+        if count('a', board1) == 0:
+            print('a lost')
+            break
         if input_move('a', board1):
             break
+
         print(board1)
+        if count('c', board1) == 0:
+            print('c lost')
+            break
         if input_move('c', board1):
             break
-play()     
+
+
+pygame.init()
+display_height = 800
+display_width = 600
+green = (0, 255, 0)
+white = (255, 255, 255)
+gameDisplay = pygame.display.set_mode((display_width, display_height))
+pygame.display.set_caption('chain reaction')
+clock = pygame.time.Clock()
+if getattr(sys, 'frozen', False):
+    os.chdir(os.path.dirname(sys.executable))
+
+
+def interactive_button(txt, x, y, w, h, ic, ac, action=None):
+    mouse = pygame.mouse.get_pos()
+    click = pygame.mouse.get_pressed()
+    if x+w > mouse[0] > x and y+h > mouse[1] > y:
+        pygame.draw.rect(gameDisplay, ac, (x, y, w, h))
+        if click[0] == 1 and action != None:
+            action()
+    else:
+        pygame.draw.rect(gameDisplay, ic, (x, y, w, h))
+    writeText(txt, textsize=int(0.8*h), x=x+w/2, y=y+h/2)
+
+
+def play_display():
+    gameExit = False
+    while not gameExit:
+        pass
